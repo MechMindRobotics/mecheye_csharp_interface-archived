933189386988a81bd715567bf1d21cf8cf731ac0
xiangru <wang.xiangru@mech-mind.net>
2021-10-09 17:17:16 +0800

    fix: The program freezes when the IP is wrong.

--- Mechmind_CameraAPI_Csharp/ZmqClient.cs
+++ Mechmind_CameraAPI_Csharp/ZmqClient.cs
@@ -7,9 +7,7 @@ namespace Mechmind_CameraAPI_Csharp
 {
     class ZmqClient
     {
-      
-           
-        const int port = 5577;
+        const int PORT = 5577;
         RequestSocket client;
         private string addr = "";
         private byte[] reqbuf;
@@ -18,16 +16,24 @@ namespace Mechmind_CameraAPI_Csharp
         { 
             client = new RequestSocket();
         }
-        public int setAddr(string ip)
+        public Status setAddr(string ip)
         {
             if (ip.Length == 0)
-                return -1;
+                return Status.Error;
             if (addr.Length != 0)
                 client.Disconnect(addr);
-            addr = "tcp://" + ip + ":" + port.ToString();
+            addr = "tcp://" + ip + ":" + PORT.ToString();
             Console.WriteLine("Connect to " + addr);
-            client.Connect(addr);
-            return 0;
+            try
+            {
+                client.Connect(addr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Network Error.Please check your IP address and network connection!");
+                return Status.Error;
+            }
+            return Status.Success;
         }
         public void disconnect()
         {
@@ -42,20 +48,16 @@ namespace Mechmind_CameraAPI_Csharp
         {
             try
             {
-                reqbuf = System.Text.Encoding.Default.GetBytes(request); ;
+                reqbuf = System.Text.Encoding.Default.GetBytes(request); 
                 client.SendFrame(reqbuf);
-                resbuf = client.ReceiveFrameBytes();
-                return resbuf;
-            }  
+                client.TryReceiveFrameBytes(TimeSpan.FromMilliseconds(Constant.TIMEOUTMS),out resbuf);
+            }
             catch (Exception e)
             {
-                Console.WriteLine("Network Error. Please check your ip address and network connection!");
+                Console.WriteLine("Receive Error. Please assure your IP address and network connection and review the camera settings!");
                 System.Environment.Exit(0);
             }
-            return null;
+            return resbuf;
         }
-        
-        
-        
     }
 }
